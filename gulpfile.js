3031af99db21bac832924cdc1cd437d991aed11c
bamboo <825391236@qq.com>
2023-05-16 15:35:43 +0800

    config: modify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-
 const gulp = require("gulp");
 const ts = require("gulp-typescript");
 const clean = require("gulp-clean");
 const tsProject = ts.createProject('tsconfig.json');
 const uglify = require("gulp-uglify");
+const gulpJsonEditor = require('gulp-json-editor');
 
 function build() {
   return tsProject.src()
@@ -13,7 +13,25 @@ function build() {
 }
 
 function cleanDist() {
-  return gulp.src("dist").pipe(clean())
+  return gulp.src("dist", { read: false, allowEmpty: true }).pipe(clean())
+}
+
+function copyLicense() {
+  return gulp.src(["./LICENSE"])
+    .pipe(gulp.dest('dist'))
+}
+
+// 复制package.json 并删除指定字段
+const removeFields = ["scripts", "devDependencies", "dependencies"]
+function copyPackageJson() {
+  return gulp.src("./package.json")
+    .pipe(gulpJsonEditor(function (json) {
+      // 删除指定的字段
+      removeFields.forEach(field => {
+        delete json[field]
+      });
+      return json;
+    })).pipe(gulp.dest('dist'))
 }
 
 // gulp.task("build", function () {
@@ -24,4 +42,4 @@ function cleanDist() {
 // });
 exports.build = build;
 exports.clean = cleanDist;
-exports.default = gulp.series(cleanDist, build)
+exports.default = gulp.series(cleanDist, gulp.parallel(copyLicense, copyPackageJson), build)
